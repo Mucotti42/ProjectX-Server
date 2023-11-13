28e244fdd3521e3e9af056aee1d59568559c166f
Mucotti42 <75278760+Mucotti42@users.noreply.github.com>
2023-11-14 00:20:23 +0300

    "userName"

--- functions/TestHandler.js
+++ functions/TestHandler.js
@@ -5,5 +5,5 @@ const communication = require('../Communication.js')
 exports.GetAge = async function(client,data){
     let dbData = await db.GetData('playerinfo', 'userName', data.value);
     console.log('GetAge' + dbData);
-    communication.SendPackage(client,'Print', dbData)
+    communication.SendPackage(client,'Print', dbData["userName"])
 }

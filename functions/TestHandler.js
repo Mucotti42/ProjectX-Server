e8ff3d308d8ac963e2c530b01d548ed1d84d1e3d
Mucotti42 <75278760+Mucotti42@users.noreply.github.com>
2023-11-14 00:59:24 +0300

    dbtables

--- functions/TestHandler.js
+++ functions/TestHandler.js
@@ -1,6 +1,6 @@
 const db = require('../database.js')
 const communication = require('../Communication.js')
-const dbFields = require('./dbTables')
+const dbFields = require('../dbTables.js')
 
 exports.GetAge = async function(client,data){
     let dbData = await db.GetData(dbFields.tableTypes.PLAYERINFO, dbFields.playerInfo.USERNAME, data.value);

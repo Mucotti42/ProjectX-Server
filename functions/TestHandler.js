5604efb02464ad353a6b1023711e5d8c874638db
Mucotti42 <75278760+Mucotti42@users.noreply.github.com>
2023-11-14 13:30:09 +0300

    .

--- functions/TestHandler.js
+++ functions/TestHandler.js
@@ -3,7 +3,7 @@ const communication = require('../Communication.js')
 const dbFields = require('../dbTables.js')
 
 exports.GetAge = async function(client,data){
-    let dbData = await db.GetData(dbFields.tableTypes.PLAYERINFO, dbFields.playerInfo.USERNAME, data.primaryKey);
+    let dbData = await db.GetData(dbFields.tableTypes.PLAYERINFO, dbFields.playerInfo.USERNAME, data.value);
     console.log('GetAge' + dbData);
     communication.SendPackage(client,'Print', dbData["userName"])
 

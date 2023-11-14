91da3e8f60af30dc5364b88f8125bcdad90fbfac
Mucotti42 <75278760+Mucotti42@users.noreply.github.com>
2023-11-14 13:17:15 +0300

    .

--- functions/TestHandler.js
+++ functions/TestHandler.js
@@ -3,17 +3,17 @@ const communication = require('../Communication.js')
 const dbFields = require('../dbTables.js')
 
 exports.GetAge = async function(client,data){
-    //let dbData = await db.GetData(dbFields.tableTypes.PLAYERINFO, dbFields.playerInfo.USERNAME, data.primaryKey);
-    //console.log('GetAge' + dbData);
-    //communication.SendPackage(client,'Print', dbData["userName"])
+    let dbData = await db.GetData(dbFields.tableTypes.PLAYERINFO, dbFields.playerInfo.USERNAME, data.primaryKey);
+    console.log('GetAge' + dbData);
+    communication.SendPackage(client,'Print', dbData["userName"])
 
     // var value =[
     //     [1,'Uzay']
     // ]
-    const userdata ={
-        primaryKey: 'UUID()',
-        userName: 'Berkay'
-    };
-    await db.InsertData(dbFields.tableTypes.PLAYERINFO,userdata);
-    console.log(await db.GetData(dbFields.tableTypes.PLAYERINFO, dbFields.playerInfo.PRIMARYKEY, userdata.userName, dbFields.playerInfo.USERNAME))
+    // const userdata ={
+    //     primaryKey: 'UUID()',
+    //     userName: 'Berkay'
+    // };
+    // await db.InsertData(dbFields.tableTypes.PLAYERINFO,userdata);
+    // console.log(await db.GetData(dbFields.tableTypes.PLAYERINFO, dbFields.playerInfo.PRIMARYKEY, userdata.userName, dbFields.playerInfo.USERNAME))
 }

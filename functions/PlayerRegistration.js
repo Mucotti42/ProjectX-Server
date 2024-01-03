9fb682f7c0ad84e6652d5f3ea99960b9d8c7fdfe
Mucotti42 <75278760+Mucotti42@users.noreply.github.com>
2024-01-03 19:25:51 +0300

    Update PlayerRegistration.js

--- functions/PlayerRegistration.js
+++ functions/PlayerRegistration.js
@@ -27,7 +27,7 @@ exports.RegisterPlayer = async function(client,data){
             userName: 'New Player',
             apiId: data.key,
             playerRank : 50,
-            characters : '[0, 1, 2, 3, 4, 5, 6, 7, 10]'
+            characters : '[0, 1, 2, 3, 4, 5, 6, 7, 8, 10]'
         };
         await db.InsertData(dbFields.tableTypes.PLAYERINFO,userdata);
         
